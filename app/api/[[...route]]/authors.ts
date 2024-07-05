4ef6c380cdc53cd1e17e634bedabf86cd7d603d8
Jamin <jaminwebdev@gmail.com>
2024-07-04 21:09:24 -0700

    dummy author route removed

--- app/api/[[...route]]/authors.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono';
-
-const app = new Hono()
-
-app.get('/', (c) => c.json({ authors: 'blah' }))
-app.post('/', (c) => c.json({ authors: 'blah created' }, 201))
-
-export default app
