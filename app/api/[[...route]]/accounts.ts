98e484a4cbf1e813aa55ef09638862042023b682
Jamin <jaminwebdev@gmail.com>
2024-07-13 20:58:09 -0700

    only return accounts for auth user

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,6 +3,7 @@ import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { HTTPException } from 'hono/http-exception'
+import { eq } from "drizzle-orm";
 
 const app = new Hono()
     .get('/', 
@@ -22,6 +23,7 @@ const app = new Hono()
                 name: accounts.name
             })
             .from(accounts)
+            .where(eq(accounts.userId, auth.userId))
             
         return c.json({ data })
     })
