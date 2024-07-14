a99437b68f505edd5a2cc66326829cee4583f51e
Jamin <jaminwebdev@gmail.com>
2024-07-13 20:56:55 -0700

    accounts API RPC setup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,50 +1,24 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import accounts from './accounts'
+import { HTTPException } from 'hono/http-exception'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    return c.json({ hello: 'world'})
+app.onError((err, c) => {
+    if(err instanceof HTTPException) {
+        return err.getResponse()
+    }
+
+    return c.json({ error: "Internal server error"})
 })
+
+const routes = app
+    .route('/accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
 
-
-// dummy examples
-// app
-// .get('/test', (c) => {
-//     return c.json({
-//         message: 'Test route'
-//     })
-// })
-// .get('/test/:id',
-//     zValidator("param", z.object({
-//         id: z.string({message: 'Please pass a string, not a number'}),
-//     })),
-//     (c) => {
-//     const { id } = c.req.valid("param")
-
-//     return c.json({
-//         id: `Here's your ${id}`
-//     })
-// })
-// .post('/create/:postId', 
-//     zValidator("json", z.object({
-//         name: z.string(),
-//         userId: z.number()
-//     })),
-//     zValidator("param", z.object({
-//         postId: z.number()
-//     })), (c) => {
-//         const { name, userId } = c.req.valid("json")
-//         const { postId } = c.req.valid("param")
-        
-//         return c.json({
-//             name,
-//             userId,
-//             postId
-//         })
-// }) 
+export type AppType = typeof routes;
