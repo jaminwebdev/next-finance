1a02b80034389b6c26916c0830b1631f918d41c2
Jamin <jaminwebdev@gmail.com>
2024-07-16 12:32:37 -0700

    new account form setup

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,9 +1,20 @@
 import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle} from "@/components/ui/sheet"
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { AccountForm } from "@/features/accounts/components/account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>
 
 export const NewAccountSheet = () => {
 
     const { isOpen, onClose } = useNewAccount();
+
+    const onSubmit = (values: FormValues) => {
+        console.log(values)
+    }
 
     return (
         <Sheet open={isOpen} onOpenChange={onClose}>
@@ -16,6 +27,12 @@ export const NewAccountSheet = () => {
                         Create a new account to track your transactions.
                     </SheetDescription>
                 </SheetHeader>
+                <AccountForm 
+                    onSubmit={onSubmit} 
+                    disabled={false}
+                    defaultValues={{
+                        name: ''
+                    }}/>
             </SheetContent>
         </Sheet>
     )
