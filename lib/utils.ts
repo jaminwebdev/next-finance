997e315a2803ae8863b5fab1d721779414ac1d19
Jamin <jaminwebdev@gmail.com>
2024-08-12 05:55:21 -0700

    new transaction form

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertAmountToMiliUnits = (amount: number) => {
+  return Math.round(amount * 1000);
+}
+
+export const convertAmountFromMiliunits = (amount: number) => {
+  return Math.round(amount / 1000);
+}
