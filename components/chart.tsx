602f24fc52f16dabe1fcc702f235c2ac06f788e2
Jamin <jaminwebdev@gmail.com>
2024-08-16 11:14:53 -0700

    add chart variants to overview chart

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,12 +1,21 @@
+import { ReactElement, ReactNode, useEffect, useState } from 'react';
+import {
+    Select,
+    SelectTrigger,
+    SelectContent,
+    SelectValue,
+    SelectItem
+} from '@/components/ui/select'
 import {
     Card,
     CardContent,
     CardHeader,
     CardTitle
 } from '@/components/ui/card'
-import { FileSearch } from 'lucide-react';
+import { AreaChart, BarChart, FileSearch, LineChart } from 'lucide-react';
 import { AreaVariant } from './area-variant';
 import { BarVariant } from './bar-variant';
+import { LineVariant } from './line-variant';
 
 type Props = {
     data?: {
@@ -17,26 +26,71 @@ type Props = {
 }
 
 export const Chart = ({ data = []}: Props) => {
+
+    const [chartType, setChartType] = useState("area")
+
+    const onTypeChange = (type: string) => {
+        setChartType(type)
+    }
+
+    const renderChart = (): ReactNode => {
+        if (chartType === 'line') return ( <LineVariant data={data} /> )
+        if (chartType === 'bar') return ( <BarVariant data={data} /> )
+        return ( <AreaVariant data={data} /> )
+    }
+
     return (
         <Card className='border-none drop-shadow-sm'>
             <CardHeader className='flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between'>
                 <CardTitle className='text-xl line-clamp-1'>
                     Transactions
                 </CardTitle>
-                {/* TODO: add select */}
+                <Select
+                    defaultValue={chartType}
+                    onValueChange={onTypeChange}
+                >
+                    <SelectTrigger className='lg:w-auto h-9 rounded-md px-3'>
+                        <SelectValue placeholder="Chart type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value='area'>
+                            <div className='flex items-center'>
+                                <AreaChart className='size-4 mr-2 shrink-0' />
+                                <p className='line-clamp-1'>
+                                    Area chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value='line'>
+                            <div className='flex items-center'>
+                                <LineChart className='size-4 mr-2 shrink-0' />
+                                <p className='line-clamp-1'>
+                                    Line chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value='bar'>
+                            <div className='flex items-center'>
+                                <BarChart className='size-4 mr-2 shrink-0' />
+                                <p className='line-clamp-1'>
+                                    Bar chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
             </CardHeader>
             <CardContent>
                 {data.length === 0 ? (
-                    <div className='flex flex-col gap-y-4 items-center justify-center h-[350px] w-full'>
-                        <FileSearch className='size-6 text-muted-foreground' />
-                        <p className='text-muted-foreground text-sm'>
-                            No data for this period
-                        </p>
-                    </div>
-                ) : (
-                    <AreaVariant data={data} />
-                    // <BarVariant data={data} />
-                )}
+                        <div className='flex flex-col gap-y-4 items-center justify-center h-[350px] w-full'>
+                            <FileSearch className='size-6 text-muted-foreground' />
+                            <p className='text-muted-foreground text-sm'>
+                                No data for this period
+                            </p>
+                        </div>
+                    ) 
+                    : renderChart() 
+                }
             </CardContent>
         </Card>
     )
