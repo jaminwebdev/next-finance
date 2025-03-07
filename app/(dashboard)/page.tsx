41ffd305eebcf103ed9dcadd6fa9b546ad56e533
Jamin <jaminwebdev@gmail.com>
2025-03-07 15:01:01 -0600

    seed dummy data if no recent data

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,46 @@
 
 import { DataCharts } from '@/components/data-charts';
 import { DataGrid } from '@/components/data-grid';
+import { useEffect, useCallback, useRef } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { useGetSummary } from '@/features/summary/api/use-get-summary';
+import { useGetCategories } from '@/features/categories/api/use-get-categories';
+import { dummyTransactions, getRandomDateInLast30Days } from '@/db/dummy-data';
+import { useBulkCreateTransactions } from '@/features/transactions/api/use-bulk-create-transactions';
 
 export default function DashboardPage() {
+	const { user } = useUser();
+	const { data, isSuccess } = useGetSummary();
+	const { data: categories } = useGetCategories();
+	const createTransactions = useBulkCreateTransactions();
+	const hasSeeded = useRef(false);
+
+	const seedDatabase = useCallback(
+		(categories: { id: string; name: string }[]) => {
+			const seedData = dummyTransactions.map((transaction) => ({
+				...transaction,
+				date: getRandomDateInLast30Days(),
+				categoryId: categories[Math.floor(Math.random() * categories.length)].id
+			}));
+			createTransactions.mutate(seedData);
+		},
+		[createTransactions]
+	);
+
+	useEffect(() => {
+		if (
+			!hasSeeded.current &&
+			user?.emailAddresses?.[0]?.emailAddress === 'jaminwebdev@gmail.com' &&
+			isSuccess &&
+			(!data.remainingAmount || (Array.isArray(data) && data.length === 0)) &&
+			categories
+		) {
+			console.log('seeding database');
+			seedDatabase(categories);
+			hasSeeded.current = true;
+		}
+	}, [isSuccess, data, user?.emailAddresses, categories, seedDatabase]);
+
 	return (
 		<div className="max-w-screen-2xl mx-auto w-full pb-10, -mt-10">
 			<DataGrid />
---
+++ db/dummy-data.ts
@@ -0,0 +1,253 @@
+export const dummyTransactions = [
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Taco Bell',
+		amount: -20000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: "McDonald's",
+		amount: -15000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Walmart',
+		amount: -300000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Amazon',
+		amount: -450000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Shell Gas Station',
+		amount: -50000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Target',
+		amount: -250000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Starbucks',
+		amount: -10000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Best Buy',
+		amount: 350000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Costco',
+		amount: 400000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Home Depot',
+		amount: 200000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Kroger',
+		amount: 150000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Subway',
+		amount: -5000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Nike Store',
+		amount: -300000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Adidas Store',
+		amount: 250000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Apple Store',
+		amount: -500000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Google Store',
+		amount: -450000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Microsoft Store',
+		amount: 400000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Chevron Gas Station',
+		amount: -60000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'BP Gas Station',
+		amount: -55000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'H&M',
+		amount: 200000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Zara',
+		amount: -250000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Old Navy',
+		amount: 150000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Gap',
+		amount: -180000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Banana Republic',
+		amount: -220000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Forever 21',
+		amount: 120000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Sephora',
+		amount: 300000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Ulta Beauty',
+		amount: -280000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Walgreens',
+		amount: -100000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'CVS Pharmacy',
+		amount: -90000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Rite Aid',
+		amount: -85000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: '7-Eleven',
+		amount: -5000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Dollar Tree',
+		amount: -20000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Family Dollar',
+		amount: -15000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Dollar General',
+		amount: -18000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Five Below',
+		amount: -25000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'TJ Maxx',
+		amount: -300000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Marshalls',
+		amount: -280000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Ross Stores',
+		amount: -250000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Burlington',
+		amount: -220000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Nordstrom',
+		amount: -400000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: "Macy's",
+		amount: -350000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'JCPenney',
+		amount: -300000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: "Kohl's",
+		amount: 250000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: "Dillard's",
+		amount: 200000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Sears',
+		amount: -150000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Neiman Marcus',
+		amount: 500000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: 'Saks Fifth Avenue',
+		amount: -450000
+	},
+	{
+		accountId: 'ptz1m4m9pc6tlttu7j4bcx3w',
+		payee: "Bloomingdale's",
+		amount: 400000
+	}
+];
+
+export const getRandomDateInLast30Days = () => {
+	const now = new Date();
+	const pastDate = new Date(now);
+	pastDate.setDate(now.getDate() - 30);
+
+	const randomDate = new Date(
+		pastDate.getTime() + Math.random() * (now.getTime() - pastDate.getTime())
+	);
+	return randomDate;
+};
